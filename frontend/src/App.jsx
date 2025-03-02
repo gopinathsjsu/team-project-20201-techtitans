38dbac72d8ef187fadda4e0440bfa44b00c8c69b
Isla Shi <isla2000@gmail.com>
2025-03-01 21:23:22 -0800

    Created login button components, login (for each user) component, and customer registration component
    Login buttons - page 1 of the figma
    
    Login for each user - can be easily reused, just change role="user" (ex. customer)
    
    Customer registration - only one registration page is needed and it's for customers. Contains an extra email field and will be used for creating users.
    
    `cd frontend` and `npm run dev` to see the work in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,37 +2,22 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
+import LoginButtons from "./components/LoginButtons"
+import Login from "./components/Login"
+import CustomerRegister from "./components/CustomerRegister"
 
+// demonstration of the first login page (buttons)
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
+		<div style={{ display: "flex", flexDirection: "column" }}>
+			<LoginButtons />
+			<div style={{ display: "flex", flexDirection: "column" }}>
+				<Login role="Admin" />
+				<Login role="Restaurant Manager" />
+				<Login role="Customer" />
+				<CustomerRegister />
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		</div>
 	)
 }
 
