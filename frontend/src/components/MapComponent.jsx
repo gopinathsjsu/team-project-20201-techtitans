12613a015dfd222ecacaa5fa823c69e9098a8df3
Isla Shi <isla2000@gmail.com>
2025-03-13 11:28:42 -0700

    Created very basic Google Maps component
    `npm install @react-google-maps/api  `

---
+++ frontend/src/components/MapComponent.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const MapComponent = () => {
+  const mapStyles = {
+    height: '400px',
+    width: '800px',
+  };
+
+  // San Jose coordinates
+  const defaultCenter = {
+    lat: 37.335480,
+    lng: -121.893028,
+  };
+
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyBeMgCkD6SV6CVSuxtbeFhW94ThNXj9-yU">
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={10}
+        center={defaultCenter}
+      />
+    </LoadScript>
+  );
+};
+
+export default MapComponent;
