c283289f675f084d7990f9f93950eb34ebac39de
Isla Shi <isla2000@gmail.com>
2025-03-13 11:31:49 -0700

    Forgot to run prettier/linter

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const MapComponent = () => {
-  const mapStyles = {
-    height: '400px',
-    width: '800px',
-  };
+	const mapStyles = {
+		height: "400px",
+		width: "800px",
+	};
 
-  // San Jose coordinates
-  const defaultCenter = {
-    lat: 37.335480,
-    lng: -121.893028,
-  };
+	// San Jose coordinates
+	const defaultCenter = {
+		lat: 37.33548,
+		lng: -121.893028,
+	};
 
-  return (
-    <LoadScript googleMapsApiKey="AIzaSyBeMgCkD6SV6CVSuxtbeFhW94ThNXj9-yU">
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={10}
-        center={defaultCenter}
-      />
-    </LoadScript>
-  );
+	return (
+		<LoadScript googleMapsApiKey="AIzaSyBeMgCkD6SV6CVSuxtbeFhW94ThNXj9-yU">
+			<GoogleMap
+				mapContainerStyle={mapStyles}
+				zoom={10}
+				center={defaultCenter}
+			/>
+		</LoadScript>
+	);
 };
 
 export default MapComponent;
