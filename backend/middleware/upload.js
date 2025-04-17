4b694700e0bcafde32801eea0d56bdebc5cabf6d
Joel Zapana <joel.zapana@sjsu.edu>
2025-04-16 18:25:26 -0700

    Ran prettier on upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,15 +1,15 @@
-import multer from 'multer';
-import path from 'path';
+import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'backend/uploads');
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname);
-    cb(null, `${uniqueSuffix}${ext}`);
-  }
+	destination: function (req, file, cb) {
+		cb(null, "backend/uploads");
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		const ext = path.extname(file.originalname);
+		cb(null, `${uniqueSuffix}${ext}`);
+	},
 });
 
 const upload = multer({ storage });
